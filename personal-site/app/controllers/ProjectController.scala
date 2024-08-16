f0c061397fc5416a2c01d00893bb0bb0e8aa12c7
Diego Cabrera <90268530+Diegcor@users.noreply.github.com>
2024-08-16 15:53:16 -0600

    created basic project controlelr class

---
+++ personal-site/app/controllers/ProjectController.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+@Singleton
+class ProjectController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+    def show(pName: String) = Action {implicit request: Request[AnyContent] => 
+        pName match {
+            case "Particle-Engine" => Ok(views.html.projectPages.particleEngine())
+        }
+    }
+}
