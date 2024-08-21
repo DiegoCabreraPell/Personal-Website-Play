2f888c5c314074dc0885057b16ff013f2a1b1c82
Diego Cabrera <90268530+Diegcor@users.noreply.github.com>
2024-08-21 13:54:46 -0600

    background color changes on card hover

--- personal-site/public/javascripts/projects.js
+++ personal-site/public/javascripts/projects.js
@@ -1,3 +1,27 @@
 document.getElementById("projects").classList.add("cur-page");
 
-document.getElementById
+const cards = document.getElementsByClassName("card");
+
+const cardColors = [
+    "#101220",
+    "#032206",
+    "#351010",
+    "#351010",
+    "#351010",
+    "#351010",
+    "#351010",
+    "#351010"
+]
+
+const numCards = 8;
+
+function changeBackgroundColor(color) {
+    document.getElementById("center-container").style.backgroundColor = color;
+}
+
+for (let cardIndex = 0; cardIndex < numCards; cardIndex++) {
+    let card = cards.item(cardIndex);
+    card.onmouseover = function() {changeBackgroundColor(cardColors[cardIndex])};
+    card.onmouseout = function() {changeBackgroundColor("#101010")};
+}
+
